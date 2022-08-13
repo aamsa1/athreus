71bd951af9b100df829a94dfb3059d5a5bd37eec
aamsa1 <ahmad.amsori@tokopedia.com>
2022-08-14 00:19:30 +0700

    Remove

--- dashboard.php
+++
@@ -1,8 +0,0 @@
-<h3>Selamat Datang di Aplikasi QR Code</h3>
-
-<h4 class="page-header">Fitur Aplikasi</h4>
-<ul>
-	<li>Menginput Code QR Code</li>
-	<li>Mencetak QR Code</li>
-	<li>Menampilkan QR Code di PDF</li>
-</ul>
